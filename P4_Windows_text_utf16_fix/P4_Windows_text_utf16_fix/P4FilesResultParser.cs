00dccfa94c5a7b713e74125f343c07d577e91a85
l10veu602 <l10veu602@gmail.com>
2017-02-28 23:59:04 +0900

    add P4_Windows_text_utf16_fix project

---
+++ P4_Windows_text_utf16_fix/P4_Windows_text_utf16_fix/P4FilesResultParser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace P4_Windows_text_utf16_fix
+{
+    static class P4FileTypes
+    {
+        public const string Text = "text";
+    }
+
+    struct P4FilesResult
+    {
+        public string DepotPath { get; set; }
+        public int Revision { get; set; }
+        public string Action { get; set; }
+        public int Changelist { get; set; }
+        public string FileType { get; set; }
+    }
+
+    class P4FilesResultParser
+    {
+        private const string DepotPathRegexString = "([^#]*)";
+        private const string RevisionRegexString = "([^ ]*)";
+        private const string ActionRegexString = "([^ ]*)";
+        private const string ChangelistRegexString = "([^ ]*)";
+        private const string FileTypeRegexString = "(.*)";
+
+        private static readonly Regex P4FilesResultRegex = new Regex(
+            $"{DepotPathRegexString}#{RevisionRegexString} - {ActionRegexString} change {ChangelistRegexString} \\({FileTypeRegexString}\\)");
+
+        public static P4FilesResult Parse(string p4FilesResultString)
+        {
+            var match = P4FilesResultRegex.Match(p4FilesResultString);
+            var groups = match.Groups;
+
+            var p4FilesResult = new P4FilesResult();
+
+            p4FilesResult.DepotPath = groups[1].Value;
+
+            string revisionString = groups[2].Value;
+            p4FilesResult.Revision = int.Parse(revisionString);
+
+            p4FilesResult.Action = groups[3].Value;
+
+            string changelistString = groups[4].Value;
+            p4FilesResult.Changelist = int.Parse(changelistString);
+
+            p4FilesResult.FileType = groups[5].Value;
+
+            return p4FilesResult;
+        }
+
+        public static IEnumerable<P4FilesResult> ParseList(IEnumerable<string> p4FilesResults)
+        {
+            return p4FilesResults.Select(result => Parse(result)).ToList();
+        }
+    }
+}
---
+++ P4_Windows_text_utf16_fix/P4_Windows_text_utf16_fix/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_Windows_text_utf16_fix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Arguments: <depot files path>");
+
+                return;
+            }
+
+            string depotFilesPath = args[0];
+
+            List<string> depotPaths = GetTextFileTypeDepotPaths(depotFilesPath);
+            Console.WriteLine($"Text type file count: {depotPaths.Count}");
+
+            int Index = 0;
+            int utf16Count = 0;
+            foreach (string depotPath in depotPaths)
+            {
+                Console.WriteLine($"{Index + 1}/{depotPaths.Count}");
+
+                try
+                {
+                    string localPath = ConvertDepotPathToLocalPath(depotPath);
+                    if (IsUTF16Encoded(localPath))
+                    {
+                        ++utf16Count;
+
+                        ChangeFileTypeToUTF16(depotPath);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Failed. depot path: {depotPath}, exception: {e.Message}");
+                }
+
+                ++Index;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Completed");
+            Console.WriteLine($"utf16 file count: {utf16Count}");
+        }
+
+        private static List<string> GetTextFileTypeDepotPaths(string depotFilesPath)
+        {
+            var textFileTypeDepotPaths = new List<string>();
+
+            foreach (var line in RunP4CommandWithResults($"files {depotFilesPath}"))
+            {
+                var p4FilesResult = P4FilesResultParser.Parse(line);
+
+                if (p4FilesResult.FileType == P4FileTypes.Text)
+                {
+                    textFileTypeDepotPaths.Add(p4FilesResult.DepotPath);
+                }
+            }
+
+            return textFileTypeDepotPaths;
+        }
+
+        private static string ConvertDepotPathToLocalPath(string depotPath)
+        {
+            int indexOfLastSlash = depotPath.LastIndexOf('/');
+            string fileName = depotPath.Substring(indexOfLastSlash + 1);
+
+            string p4WhereResult = RunP4CommandWithResults($"where \"{depotPath}\"").Last();
+            int firstFileNameIndex = p4WhereResult.IndexOf(fileName);
+            int secondFileNameIndex = p4WhereResult.IndexOf(fileName, firstFileNameIndex + fileName.Length);
+
+            string localPath = p4WhereResult.Substring(secondFileNameIndex + fileName.Length + 1);
+
+            return localPath;
+        }
+
+        private static bool IsUTF16Encoded(string path)
+        {
+            var encoding = SimpleHelpers.FileEncoding.DetectFileEncoding(path);
+
+            return encoding == Encoding.Unicode || encoding == Encoding.BigEndianUnicode;
+        }
+
+        private static void ChangeFileTypeToUTF16(string depotPath)
+        {
+            RunP4Command($"edit -t utf16 \"{depotPath}\"");
+        }
+
+        private static IEnumerable<string> RunP4CommandWithResults(string args)
+        {
+            var processStartInfo = new System.Diagnostics.ProcessStartInfo("p4", args);
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.RedirectStandardOutput = true;
+
+            var process = System.Diagnostics.Process.Start(processStartInfo);
+            while (true)
+            {
+                string line = process.StandardOutput.ReadLine();
+                if (line == null)
+                {
+                    yield break;
+                }
+
+                yield return line;
+            }
+        }
+
+        private static void RunP4Command(string args)
+        {
+            RunP4CommandWithResults(args).ToList();
+        }
+    }
+}
